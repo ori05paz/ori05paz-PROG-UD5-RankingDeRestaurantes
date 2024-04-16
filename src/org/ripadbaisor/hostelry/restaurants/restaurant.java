df51b348db7af57650c1c3918475ac311a8c5963
ori05paz <159430429+ori05paz@users.noreply.github.com>
2024-04-16 13:56:45 +0100

    Attributes

---
+++ src/org/ripadbaisor/hostelry/restaurants/restaurant.java
@@ -0,0 +1,9 @@
+package org.ripadbaisor.hostelry.restaurants;
+
+public class restaurant {
+    
+    private String name;
+    private String location;
+    private int schedule;
+    private float score;
+}
