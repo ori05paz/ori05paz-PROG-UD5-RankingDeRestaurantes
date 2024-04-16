0946d5eff1f61cad02e6449c34dabd7674c483c4
ori05paz <159430429+ori05paz@users.noreply.github.com>
2024-04-16 14:01:34 +0100

    Constructors

--- src/org/ripadbaisor/hostelry/restaurants/restaurant.java
+++ src/org/ripadbaisor/hostelry/restaurants/restaurant.java
@@ -6,4 +6,14 @@ public class restaurant {
     private String location;
     private int schedule;
     private float score;
+
+
+    public restaurant(String name, String location, int schedule, float score) {
+        this.name = name;
+        this.location = location;
+        this.schedule = schedule;
+        this.score = score;
+    }
+
+    
 }
