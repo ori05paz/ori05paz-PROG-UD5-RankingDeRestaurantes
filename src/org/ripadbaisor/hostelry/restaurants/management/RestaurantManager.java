0d1c265d6baeee939427cb5e7264d96cc4ec514b
ori05paz <159430429+ori05paz@users.noreply.github.com>
2024-04-19 17:56:27 +0100

    feat(Método para agregar un restaurante)

--- src/org/ripadbaisor/hostelry/restaurants/management/RestaurantManager.java
+++ src/org/ripadbaisor/hostelry/restaurants/management/RestaurantManager.java
@@ -18,4 +18,20 @@ public class RestaurantManager {
         }
     }
 
+    public static void addRestaurant(String name, String location, String schedule, float score) {
+        Restaurant restaurant = new Restaurant(name, location, schedule, score);
+        restaurants.add(restaurant);
+        JOptionPane.showMessageDialog(null, "Restaurante añadido correctamente.");
+    }
+
+    public static void addRestaurant() {
+        String name = askMessage("Ingrese el nombre del restaurante:");
+        String location = askMessage("Ingrese la localización del restaurante:");
+        String schedule = askMessage("Ingrese el horario del restaurante:");
+        float score = askNumber("Ingrese la puntuación del restaurante (entre 0 y 10):");
+
+
+        addRestaurant(name, location, schedule, score);
+    }
+
 }
