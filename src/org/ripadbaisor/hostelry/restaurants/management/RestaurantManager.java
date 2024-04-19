241b4763729b5addfefa470c1a908c45aec1db92
ori05paz <159430429+ori05paz@users.noreply.github.com>
2024-04-19 18:42:41 +0100

    feat(Mejoración del metodo para añadir un restaurante)

--- src/org/ripadbaisor/hostelry/restaurants/management/RestaurantManager.java
+++ src/org/ripadbaisor/hostelry/restaurants/management/RestaurantManager.java
@@ -7,30 +7,11 @@ import javax.swing.JOptionPane;
 public class RestaurantManager {
     private static ArrayList<Restaurant> restaurants = new ArrayList<>();
 
-    public static String askMessage(String message) {
-        return JOptionPane.showInputDialog(message);
-    }
-
-    public static int askNumber(String message) {
-        while (true) {
-            String input = JOptionPane.showInputDialog(message);
-                return Integer.parseInt(input);
-        }
-    }
-
-    public static void addRestaurant(String name, String location, String schedule, float score) {
-        Restaurant restaurant = new Restaurant(name, location, schedule, score);
-        restaurants.add(restaurant);
-        JOptionPane.showMessageDialog(null, "Restaurante añadido correctamente.");
-    }
-
-    public static void addRestaurant() {
-        String name = askMessage("Ingrese el nombre del restaurante:");
-        String location = askMessage("Ingrese la localización del restaurante:");
-        String schedule = askMessage("Ingrese el horario del restaurante:");
-        float score = askNumber("Ingrese la puntuación del restaurante (entre 0 y 10):");
-
-
+    public static void addRestaurant(String name2, String location2, String schedule2, int score2) {
+        String name = JOptionPane.showInputDialog("Ingrese el nombre del restaurante:");
+        String location = JOptionPane.showInputDialog("Ingrese la localización del restaurante:");
+        String schedule = JOptionPane.showInputDialog("Ingrese el horario del restaurante:");
+        int score = Integer.parseInt(JOptionPane.showInputDialog("Ingrese la puntuación del restaurante (entre 0 y 10):"));
         addRestaurant(name, location, schedule, score);
     }
 
