0582ad8ef3daaa358463436c9e254d5434582e5b
ori05paz <159430429+ori05paz@users.noreply.github.com>
2024-04-19 17:43:51 +0100

    feat(Creación de método para solicitar un string)

--- src/org/ripadbaisor/hostelry/restaurants/management/RestaurantManager.java
+++ src/org/ripadbaisor/hostelry/restaurants/management/RestaurantManager.java
@@ -1,11 +1,14 @@
 package org.ripadbaisor.hostelry.restaurants.management;
 
 import java.util.ArrayList;
+import javax.swing.JOptionPane;
 
 
 public class RestaurantManager {
     private static ArrayList<Restaurant> restaurants = new ArrayList<>();
 
+    public static String askMessage(String message) {
+        return JOptionPane.showInputDialog(message);
+    }
 
-    
 }
