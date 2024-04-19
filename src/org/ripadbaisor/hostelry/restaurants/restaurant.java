bf4d31c604607a150819b11f9f2cabf4851a3834
ori05paz <159430429+ori05paz@users.noreply.github.com>
2024-04-19 16:41:19 +0100

    feat(Creacion de getters y setters)

--- src/org/ripadbaisor/hostelry/restaurants/restaurant.java
+++ src/org/ripadbaisor/hostelry/restaurants/restaurant.java
@@ -1,17 +1,48 @@
 package org.ripadbaisor.hostelry.restaurants;
 
 public class restaurant {
-    
+
     private String name;
     private String location;
     private int schedule;
     private float score;
 
-
     public restaurant(String name, String location, int schedule, float score) {
         this.name = name;
         this.location = location;
         this.schedule = schedule;
+        this.score = score;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public int getSchedule() {
+        return schedule;
+    }
+
+    public void setSchedule(int schedule) {
+        this.schedule = schedule;
+    }
+
+    public float getScore() {
+        return score;
+    }
+
+    public void setScore(float score) {
         this.score = score;
     }
 
