d10b6631fce8878503f3c0bc4367a993140c9bc1
ori05paz <159430429+ori05paz@users.noreply.github.com>
2024-04-19 17:15:45 +0100

    feat(Creación del ArrayList. También tuve que volver a hacer la clase "Restaurant.java")

---
+++ src/org/ripadbaisor/hostelry/restaurants/management/RestaurantManager.java
@@ -0,0 +1,7 @@
+package org.ripadbaisor.hostelry.restaurants.management;
+
+import java.util.ArrayList;
+
+public class RestaurantManager {
+    private static ArrayList<Restaurant> restaurants = new ArrayList<>();
+}
