7ae61dab2da6b730e701f3cc0d16e1f7a371dbf4
ori05paz <159430429+ori05paz@users.noreply.github.com>
2024-04-19 19:45:09 +0100

    feat(Modificar el menu con JOptionPane)

--- src/App.java
+++ src/App.java
@@ -1,5 +1,32 @@
+import javax.swing.JOptionPane;
+
+import org.ripadbaisor.hostelry.restaurants.management.RestaurantManager;
+import org.ripadbaisor.hostelry.restaurants.util.Menu;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        int option;
+        do {
+            option = Menu.showMenu();
+                switch (option) {
+                    case 1:
+                        RestaurantManager.addRestaurant();
+                        break;
+                    case 2:
+                        RestaurantManager.updateRestaurant();
+                        break;
+                    case 3:
+                        RestaurantManager.showRestaurants();
+                        break;
+                    case 4:
+                        RestaurantManager.deleteRestaurants();
+                        break;
+                    case 5:
+                        JOptionPane.showMessageDialog(null, "Saliendo del programa...");
+                        break;
+                    default:
+                        JOptionPane.showMessageDialog(null, "Opción inválida. Por favor, elige una opción válida.");
+                }
+        } while (option != 5);
     }
 }
